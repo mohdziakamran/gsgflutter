a8a3311a4cdbbb6dd42debc2f8b2c8f9ded95164
Mohd Zia Kamran <humobacillus@gmail.com>
2021-12-19 17:27:48 +0530

    debug layout border added and commected

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 import 'homepage/my_home_page.dart';
 
@@ -12,6 +13,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // debugPaintSizeEnabled = true;
     return MaterialApp(
       theme: ThemeData(
         primarySwatch: Colors.green,
